abb4a72e0b020f84385a7c6d75b90022ce183218
Armin <arminonline71@gmail.com>
2022-06-22 16:35:52 +0430

    Update settings.py

--- teslabank/settings.py
+++ teslabank/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ro@s9=wf+w&v6q2swl_9h9ts6g(u67gutr8(h@!0eum2y1bbcs'
+SECRET_KEY = '****'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['***']
 
 
 # Application definition
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'teslabank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {'default': {'ENGINE':'django.db.backends.postgresql_psycopg2', 'NAME':'teslabank', 'USER':'postgres', 'PASSWORD':'123456', 'HOST':'localhost', 'PORT':'5432'}}
+DATABASES = {'default': {'ENGINE':'django.db.backends.postgresql_psycopg2', 'NAME':'***', 'USER':'***', 'PASSWORD':'***', 'HOST':'***', 'PORT':'***'}}
 
 
 # Password validation
